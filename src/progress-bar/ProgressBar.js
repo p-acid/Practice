9599611cb398704fc758bef796b1d83419924e2c
p-acid <qkrtkstjd01@gmail.com>
2021-11-03 17:47:26 +0900

    [Add] Practice of progress bar

--- src/progress-bar/ProgressBar.js
+++ src/progress-bar/ProgressBar.js
@@ -2,30 +2,60 @@ import React, { useState } from "react";
 import styled from "styled-components";
 
 const ProgressBar = () => {
-  const [percent, setPercent] = useState(70);
+  const [currentTag, setCurrentTag] = useState([]);
+
+  const len = currentTag.length;
+
+  const handleCurrentTag = id => {
+    if (currentTag.includes(id)) {
+      setCurrentTag(prev => {
+        const arr = [...prev];
+        arr.splice(prev.indexOf(id), 1);
+        return arr;
+      });
+    } else if (len < 5) {
+      setCurrentTag(prev => [...prev, id]);
+    }
+  };
+
+  const percent = len * 20;
 
   return (
     <Wrapper>
-      <Title>Progress Bar</Title>
+      <Title>Keyword Selector</Title>
+      <SubTitle>Selected Keyword</SubTitle>
+      {len > 0 ? (
+        <TagBox>
+          {EXAMPLE.map(item => {
+            const { id, tagName } = item;
+            return (
+              currentTag.includes(id) && (
+                <Tag key={id} onClick={() => handleCurrentTag(id)}>
+                  {tagName}
+                </Tag>
+              )
+            );
+          })}
+        </TagBox>
+      ) : (
+        <None>None</None>
+      )}
+      <SubTitle>Keyword List</SubTitle>
+      <TagBox>
+        {EXAMPLE.map(item => {
+          const { id, tagName } = item;
+          return (
+            !currentTag.includes(id) && (
+              <Tag key={id} onClick={() => handleCurrentTag(id)}>
+                {tagName}
+              </Tag>
+            )
+          );
+        })}
+      </TagBox>
       <BarWrapper>
         <Bar percent={percent}>{percent}%</Bar>
       </BarWrapper>
-      <div>
-        <Button
-          onClick={() => {
-            setPercent(percent + 5);
-          }}
-        >
-          up
-        </Button>
-        <Button
-          onClick={() => {
-            setPercent(percent - 5);
-          }}
-        >
-          down
-        </Button>
-      </div>
     </Wrapper>
   );
 };
@@ -37,26 +67,104 @@ const Wrapper = styled.div`
   align-items: center;
   flex-direction: column;
   border: 1px solid gray;
+  margin: 0 auto;
+  padding: 10px;
+  width: 600px;
 `;
 
 const Title = styled.h1`
   font-size: 16px;
+  font-weight: bold;
+`;
+
+const SubTitle = styled.h2`
+  font-size: 12px;
+`;
+
+const None = styled.div`
+  padding: 10px;
+  width: 400px;
+  border: 1px solid lightgray;
+  border-radius: 5px;
+  text-align: center;
+  font-size: 14px;
+  color: gray;
+`;
+
+const TagBox = styled.section`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 5px;
+  margin-bottom: 10px;
+  padding: 10px;
+  width: 400px;
+  border: 1px solid lightgray;
+  border-radius: 5px;
+`;
+
+const Tag = styled.div`
+  padding: 6px;
+  border: 1px solid lightgray;
+  border-radius: 5px;
+  background-color: ${({ activate }) => activate && "orange"};
+  cursor: pointer;
+  transition: 1s;
 `;
 
 const BarWrapper = styled.div`
   width: 200px;
   border-radius: 10px;
   background-color: lightgray;
+  cursor: pointer;
 `;
 
 const Bar = styled.div`
   width: ${({ percent }) => percent + "%"};
   border-radius: inherit;
   text-align: center;
-  background-color: orange;
+  background-color: ${({ percent }) => (percent >= 60 ? "orange" : "gray")};
   transition: 1s;
 `;
 
-const Button = styled.button`
-  margin: 10px;
-`;
+const EXAMPLE = [
+  {
+    id: 1,
+    tagName: "키워드1",
+  },
+  {
+    id: 2,
+    tagName: "키워드2",
+  },
+  {
+    id: 3,
+    tagName: "키워드3",
+  },
+  {
+    id: 4,
+    tagName: "키워드4",
+  },
+  {
+    id: 5,
+    tagName: "키워드5",
+  },
+  {
+    id: 6,
+    tagName: "키워드6",
+  },
+  {
+    id: 7,
+    tagName: "키워드7",
+  },
+  {
+    id: 8,
+    tagName: "키워드8",
+  },
+  {
+    id: 9,
+    tagName: "키워드9",
+  },
+  {
+    id: 10,
+    tagName: "키워드10",
+  },
+];
